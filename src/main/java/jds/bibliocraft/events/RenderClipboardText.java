455d83db7d8e98c7fddd4b3a1977f5c3b033b310
Lonelyxiya <151129412+LonelyxiyaOVO@users.noreply.github.com>
2026-08-16 13:04:53 +0800

    Fix

--- src/main/java/jds/bibliocraft/events/RenderClipboardText.java
+++ src/main/java/jds/bibliocraft/events/RenderClipboardText.java
@@ -4,16 +4,21 @@ import jds.bibliocraft.Config;
 import jds.bibliocraft.items.ItemClipboard;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumHandSide;
+import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.event.RenderSpecificHandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class RenderClipboardText 
 {
 	// thanks to Vazkii and https://github.com/Vazkii/Botania/blob/master/src/main/java/vazkii/botania/client/core/handler/RenderLexicon.java for figuring this out
+	private static final float HAND_TEXT_SCALE = 0.0036F;
+	private static final double TEXT_SPACING = -0.0658D;
 	
 	@SubscribeEvent
 	public void renderItem(RenderSpecificHandEvent event)
@@ -45,43 +50,51 @@ public class RenderClipboardText
     		String pagenum = "page"+currentPage;
     		NBTTagCompound pagetag = cliptags.getCompoundTag(pagenum);
     		if (pagetag != null)
-    		{
-    			NBTTagCompound tasks = pagetag.getCompoundTag("tasks");
-    			if (tasks != null && event.getEquipProgress() == 0.0 && event.getSwingProgress() == 0.0)
-    			{
-    				renderText(mc, pagetag.getString("title"), isRightHand, 0);
-    				renderText(mc, tasks.getString("task1"), isRightHand, 1);
-    				renderText(mc, tasks.getString("task2"), isRightHand, 2);
-    				renderText(mc, tasks.getString("task3"), isRightHand, 3);
-    				renderText(mc, tasks.getString("task4"), isRightHand, 4);
-    				renderText(mc, tasks.getString("task5"), isRightHand, 5);
-    				renderText(mc, tasks.getString("task6"), isRightHand, 6);
-    				renderText(mc, tasks.getString("task7"), isRightHand, 7);
-    				renderText(mc, tasks.getString("task8"), isRightHand, 8);
-    				renderText(mc, tasks.getString("task9"), isRightHand, 9);
-    			}
-    		}
-    	}
+			{
+				NBTTagCompound tasks = pagetag.getCompoundTag("tasks");
+				if (tasks != null && event.getEquipProgress() == 0.0 && event.getSwingProgress() == 0.0)
+				{
+					renderText(mc, event, stack, pagetag.getString("title"), isRightHand, 0);
+					renderText(mc, event, stack, tasks.getString("task1"), isRightHand, 1);
+					renderText(mc, event, stack, tasks.getString("task2"), isRightHand, 2);
+					renderText(mc, event, stack, tasks.getString("task3"), isRightHand, 3);
+					renderText(mc, event, stack, tasks.getString("task4"), isRightHand, 4);
+					renderText(mc, event, stack, tasks.getString("task5"), isRightHand, 5);
+					renderText(mc, event, stack, tasks.getString("task6"), isRightHand, 6);
+					renderText(mc, event, stack, tasks.getString("task7"), isRightHand, 7);
+					renderText(mc, event, stack, tasks.getString("task8"), isRightHand, 8);
+					renderText(mc, event, stack, tasks.getString("task9"), isRightHand, 9);
+				}
+			}
+		}
 	}
 	
-	private void renderText(Minecraft mc, String text, boolean isMainHand, int verticlepos)
+	private void renderText(Minecraft mc, RenderSpecificHandEvent event, ItemStack stack, String text,
+			boolean isRightHand, int verticalPos)
 	{
 		GlStateManager.pushMatrix();
-		double handOffsett = 0;
-		if (!isMainHand)
-		{
-			handOffsett = -1.062;
-		}
-		double titleOffset = 0;
-		if (verticlepos == 0)
-		{
-			titleOffset = 0.03;
-		}
-		GlStateManager.translate(0.397 - titleOffset + handOffsett, -0.105 - (0.0326 * verticlepos), -0.64);
-		GlStateManager.rotate(180f, 0.0f, 1.0f, 0.0f);
-		GlStateManager.rotate(180f, 0.0f, 0.0f, 1.0f);
-		GlStateManager.scale(0.00225, 0.00225, 0.00225);
-		mc.fontRenderer.drawString(text, 0, 0, 0x000000, false); 
+		applyFirstPersonItemTransform(mc, event, stack, isRightHand);
+		double y = verticalPos == 0 ? 0.825D : 0.76D + TEXT_SPACING * (verticalPos - 1);
+		double z = verticalPos == 0 ? 0.5D : 0.222D;
+		GlStateManager.translate(0.037D, y, z);
+		GlStateManager.rotate(270.0F, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate(180.0F, 0.0F, 0.0F, 1.0F);
+		GlStateManager.scale(HAND_TEXT_SCALE, HAND_TEXT_SCALE, HAND_TEXT_SCALE);
+		int textX = verticalPos == 0 ? -mc.fontRenderer.getStringWidth(text) / 2 : 0;
+		mc.fontRenderer.drawString(text, textX, 0, 0x000000, false); 
 		GlStateManager.popMatrix();
+	}
+
+	private void applyFirstPersonItemTransform(Minecraft mc, RenderSpecificHandEvent event, ItemStack stack,
+			boolean isRightHand)
+	{
+		int handSign = isRightHand ? 1 : -1;
+		GlStateManager.translate(handSign * 0.56F, -0.52F + event.getEquipProgress() * -0.6F, -0.72F);
+
+		IBakedModel model = mc.getRenderItem().getItemModelWithOverrides(stack, mc.world, mc.player);
+		ItemCameraTransforms.TransformType transformType = isRightHand
+				? ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND
+				: ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND;
+		ForgeHooksClient.handleCameraTransforms(model, transformType, !isRightHand);
 	}
 }
